8a1d8f273b28083f77df1aa0104c0f7a09611b63
Dmitriy Nesterovich <abcd-xyz@list.ru>
2023-09-11 20:58:18 +0700

    springboot added

---
+++ src/main/java/home/prozetx/lernenwor/LernenworApplication.java
@@ -0,0 +1,11 @@
+package home.prozetx.lernenwor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LernenworApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(LernenworApplication.class, args);
+	}
+}
---
+++ src/test/java/home/prozetx/lernenwor/LernenworApplicationTests.java
@@ -0,0 +1,13 @@
+package home.prozetx.lernenwor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LernenworApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
