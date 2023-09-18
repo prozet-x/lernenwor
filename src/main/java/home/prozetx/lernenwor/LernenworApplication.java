6bdf6157d8d706092e540419848eea18d8fd1ace
Dmitriy Nesterovich <abcd-xyz@list.ru>
2023-09-18 20:56:10 +0700

    HW

--- src/main/java/home/prozetx/lernenwor/LernenworApplication.java
+++ src/main/java/home/prozetx/lernenwor/LernenworApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LernenworApplication {
 	public static void main(String[] args) {
+		System.out.println("HELLO< WORLD!!!");
 		SpringApplication.run(LernenworApplication.class, args);
 	}
 }
