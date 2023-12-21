22cc8509c8225807d1f5f96e7923bbaeb0b00f2a
Dmitriy Nesterovich <abcd-xyz@list.ru>
2023-12-21 21:39:40 +0700

    some added

--- src/main/java/home/prozetx/lernenwor/dto/UserCreation.java
+++ src/main/java/home/prozetx/lernenwor/dto/UserCreation.java
@@ -3,6 +3,8 @@ package home.prozetx.lernenwor.dto;
 public record UserCreation(
         String name,
         String email,
-        String emailConfirm
+        String emailConfirm,
+        String password,
+        String passwordConfirm
 ) {
 }
