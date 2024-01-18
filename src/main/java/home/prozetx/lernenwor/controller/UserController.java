198327601ab49f361825034407d7458a72a97655
Dmitriy Nesterovich <abcd-xyz@list.ru>
2024-01-18 21:13:20 +0700

    retention username added

--- src/main/java/home/prozetx/lernenwor/controller/UserController.java
+++ src/main/java/home/prozetx/lernenwor/controller/UserController.java
@@ -25,13 +25,13 @@ public class UserController {
         return Long.toString(userRepository.count());
     }
     @PostMapping
-    public String createNew(@RequestBody @Valid UserCreation userCreation, BindingResult bindingResult) {
+    public String createNew(@RequestBody @Valid UserCreation userCreation /*, BindingResult bindingResult*/) {
         //userRepository.save(user);
-        if (bindingResult.hasErrors()) {
-            return bindingResult.getAllErrors().stream()
-                    .map(ObjectError::getDefaultMessage)
-                    .collect(Collectors.joining("\n"));
-        }
+//        if (bindingResult.hasErrors()) {
+//            return bindingResult.getAllErrors().stream()
+//                    .map(ObjectError::getDefaultMessage)
+//                    .collect(Collectors.joining("\n"));
+//        }
 
         return "Cool!";
     }
