140e86a74db33a96c5b23d969df0f364976ea9d8
Dima <abcd-xyz@list.ru>
2024-01-19 00:51:38 +0700

    Annotation message fixed

--- src/main/java/home/prozetx/lernenwor/controller/UserController.java
+++ src/main/java/home/prozetx/lernenwor/controller/UserController.java
@@ -5,10 +5,14 @@ import home.prozetx.lernenwor.domain.user.User;
 import home.prozetx.lernenwor.repository.UserRepository;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @AllArgsConstructor
@@ -25,14 +29,17 @@ public class UserController {
         return Long.toString(userRepository.count());
     }
     @PostMapping
-    public String createNew(@RequestBody @Valid UserCreation userCreation /*, BindingResult bindingResult*/) {
+    public ResponseEntity<?> createNew(@RequestBody @Valid UserCreation userCreation , BindingResult bindingResult) {
         //userRepository.save(user);
-//        if (bindingResult.hasErrors()) {
-//            return bindingResult.getAllErrors().stream()
-//                    .map(ObjectError::getDefaultMessage)
-//                    .collect(Collectors.joining("\n"));
-//        }
+        Map<String, Object> result = new HashMap<>();
+        if (bindingResult.hasErrors()) {
+            Map<String, String> errors = bindingResult.getFieldErrors().stream()
+                    .filter(fieldError -> fieldError.getDefaultMessage() != null)
+                    .collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage));
+            result.put("errors", errors);
+            return ResponseEntity.badRequest().body(result);
+        }
 
-        return "Cool!";
+        return ResponseEntity.ok(result);
     }
 }
