2445f8bf28610311f2c03cd69348780a27e9771b
Dmitriy Nesterovich <abcd-xyz@list.ru>
2024-04-25 14:38:26 +0700

    controller user chenged

--- src/main/java/home/prozetx/lernenwor/controller/UserController.java
+++ src/main/java/home/prozetx/lernenwor/controller/UserController.java
@@ -30,7 +30,6 @@ public class UserController {
         var result = new HashMap<String, Object>();
         result.put("users", userRepository.findAll());
         result.put("tokens", emailConfirmTokenRepository.findAll());
-        result.put("tests", emailConfirmTokenRepository.findAll());
 
         return ResponseEntity.ok(result);
     }
