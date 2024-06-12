6ae481d1a6b0f8c3c4a915d6aae3bfa0312d3a24
Dmitriy Nesterovich <abcd-xyz@list.ru>
2024-06-12 19:01:16 +0700

    test added

--- src/test/java/home/prozetx/lernenwor/LernenworApplicationTests.java
+++ src/test/java/home/prozetx/lernenwor/LernenworApplicationTests.java
@@ -1,13 +1,9 @@
 package home.prozetx.lernenwor;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest
 class LernenworApplicationTests {
-
 	@Test
 	void contextLoads() {
         assertTrue(true);
