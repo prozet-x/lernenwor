f9e5c76b9b52069cf36324094823ae344f54ca75
Dmitriy Nesterovich <abcd-xyz@list.ru>
2024-07-16 20:01:26 +0700

    security fixed

---
+++ src/main/java/home/prozetx/lernenwor/config/CustomUserDetailsService.java
@@ -0,0 +1,21 @@
+package home.prozetx.lernenwor.config;
+
+import home.prozetx.lernenwor.domain.user.User;
+import home.prozetx.lernenwor.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Optional;
+
+public class CustomUserDetailsService implements UserDetailsService {
+    UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByName(username);
+        if (user.isEmpty()) {
+            throw new UsernameNotFoundException(String.format("User with name %s does not exist", username));
+        }
+        return null;
+    }
+}
--- src/main/java/home/prozetx/lernenwor/repository/UserRepository.java
+++ src/main/java/home/prozetx/lernenwor/repository/UserRepository.java
@@ -4,8 +4,11 @@ import home.prozetx.lernenwor.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByName(String name);
     boolean existsByEmail(String email);
+    Optional<User> findByName(String name);
 }
